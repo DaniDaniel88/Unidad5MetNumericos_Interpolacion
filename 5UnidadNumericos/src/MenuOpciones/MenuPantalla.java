33f852d7bdfefdf9e3838435ad8a2642c7d1d5e6
DanyEl <danielsmith6669@hotmail.com>
2019-06-09 20:57:37 -0500

    Creacion de interfaz del metodo de lagrange

--- 5UnidadNumericos/src/MenuOpciones/MenuPantalla.java
+++ 5UnidadNumericos/src/MenuOpciones/MenuPantalla.java
@@ -27,21 +27,148 @@ public class MenuPantalla extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Titulo6 = new javax.swing.JLabel();
+        JLabelgrado = new javax.swing.JLabel();
+        txtgrado = new javax.swing.JTextField();
+        jLabelDatoX = new javax.swing.JLabel();
+        datoXCombo = new javax.swing.JComboBox<>();
+        jbCalcular = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        Tablita1 = new javax.swing.JTable();
+        jLabelResultado = new javax.swing.JLabel();
+        txtresultadoFinal = new javax.swing.JTextField();
+        ButtonReiniciar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Titulo6.setFont(new java.awt.Font("Sitka Text", 3, 18)); // NOI18N
+        Titulo6.setText("Interpolacion de Lagrange");
+
+        JLabelgrado.setText("Grado:");
+
+        jLabelDatoX.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabelDatoX.setText("Seleccione valor de X:");
+
+        datoXCombo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4" }));
+
+        jbCalcular.setBackground(new java.awt.Color(204, 255, 0));
+        jbCalcular.setText("CALCULAR");
+        jbCalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbCalcularActionPerformed(evt);
+            }
+        });
+
+        Tablita1.setForeground(new java.awt.Color(51, 255, 51));
+        Tablita1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "i", "X", "Fx()", "...."
+            }
+        ));
+        jScrollPane1.setViewportView(Tablita1);
+
+        jLabelResultado.setText("RESULTADO:");
+
+        ButtonReiniciar.setBackground(new java.awt.Color(51, 255, 204));
+        ButtonReiniciar.setText("NUEVO");
+        ButtonReiniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonReiniciarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(JLabelgrado, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtgrado, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabelDatoX)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(datoXCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Titulo6)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(8, 8, 8)
+                            .addComponent(jLabelResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(txtresultadoFinal, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(33, 33, 33)
+                            .addComponent(ButtonReiniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jbCalcular)))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(Titulo6, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JLabelgrado, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtgrado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelDatoX)
+                    .addComponent(datoXCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(25, 25, 25)
+                .addComponent(jbCalcular)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(48, 48, 48)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtresultadoFinal, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ButtonReiniciar))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCalcularActionPerformed
+
+        try {
+
+            METLAGRANGE met = new METLAGRANGE();
+            int i = Integer.parseInt(this.txtgrado.getText());
+            if (i>=2 && i<=4) {
+                //double x=Double.parseDouble(this.inter.getText());
+                String categoria1 = (String) datoXCombo.getSelectedItem();
+                //convetir dato a int
+                int datoX = (int) Double.parseDouble(categoria1);
+                System.out.println("datoX: "+ datoX);
+
+                ArrayList<VariablesLagrange> a=met.MetodoInterLagrange(i,datoX);
+                ModeloTablaLagrange m = new ModeloTablaLagrange(a);
+
+                //ModeloTablaL m=new ModeloTablaL(e);
+                this.Tablita1.setModel(m);
+                this.txtresultadoFinal.setText(met.resultado());
+            }
+            else {
+                JOptionPane.showMessageDialog(null,"Dato fuera del rango 2-4");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error al insertar dato, calcular de nuevo" + e);
+        }
+    }//GEN-LAST:event_jbCalcularActionPerformed
+
+    private void ButtonReiniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonReiniciarActionPerformed
+        txtgrado.setText("");
+        txtresultadoFinal.setText("");
+    }//GEN-LAST:event_ButtonReiniciarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +206,16 @@ public class MenuPantalla extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButtonReiniciar;
+    private javax.swing.JLabel JLabelgrado;
+    private javax.swing.JTable Tablita1;
+    private javax.swing.JLabel Titulo6;
+    private javax.swing.JComboBox<String> datoXCombo;
+    private javax.swing.JLabel jLabelDatoX;
+    private javax.swing.JLabel jLabelResultado;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbCalcular;
+    private javax.swing.JTextField txtgrado;
+    private javax.swing.JTextField txtresultadoFinal;
     // End of variables declaration//GEN-END:variables
 }
