2bc3de145acc7e520363fc4db3c29e14b140f49a
DanyEl <danielsmith6669@hotmail.com>
2019-06-09 21:03:54 -0500

    Creacion de las variables del metodo

---
+++ 5UnidadNumericos/src/MenuOpciones/VariablesLagrange.java
@@ -0,0 +1,37 @@
+
+package MenuOpciones;
+
+public class VariablesLagrange {
+    private double i, x, fx;
+
+    public double getI() 
+    {
+        return i;
+    }
+
+    public void setI(double i) 
+    {
+        this.i = i;
+    }
+
+    public double getX() 
+    {
+        return x;
+    }
+
+    public void setX(double x) 
+    {
+        this.x = x;
+    }
+
+    public double getFx() 
+    {
+        return fx;
+    }
+
+    public void setFx(double fx) 
+    {
+        this.fx = fx;
+    }
+    
+}
